f31e0f5b86b4d0f66ba9b821a70776be881793ad
liumapp <liumapp.com@gmail.com>
2018-10-29 10:08:10 +0800

    add commonValidator.vue and index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file index.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/10/29
+ */
+
+export { default } from './commonValidator.vue'
+
