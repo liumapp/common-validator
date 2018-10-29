d2ba9c32fbf0815cb83e1bf2156b880485ebb130
liumapp <liumapp.com@gmail.com>
2018-10-29 11:00:11 +0800

    add phone and idcard validator

--- index.js
+++ index.js
@@ -5,6 +5,24 @@
  * @homepage http://www.liumapp.com
  * @date 2018/10/29
  */
+import idCard from './lib/IdCard';
+import phone from './lib/Phone';
 
-export { default } from './commonValidator.vue'
+var commonValidator = {};
 
+commonValidator.install = function (Vue) {
+
+  Vue.prototype.$commonValidator['phoneValidator'] = function (rule, value, callback) {
+    if (!value) {
+      return callback(new Error('手机号不能为空'));
+    } else if (!phone.rule.test(value)) {
+      callback('手机号格式不正确');
+    } else {
+      callback();
+    }
+  }
+
+}
+
+
+module.exports = commonValidator;
