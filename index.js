c8f958cf5d47c214c77dc5db5d23e7a67dea6649
liumapp <liumapp.com@gmail.com>
2018-10-29 11:41:49 +0800

    use phone validator success

--- index.js
+++ index.js
@@ -6,23 +6,64 @@
  * @date 2018/10/29
  */
 import idCard from './lib/IdCard';
-import phone from './lib/Phone';
+import phoneValidator from './lib/phoneValidator';
 
-var commonValidator = {};
+let commonValidator = {};
 
-commonValidator.install = function (Vue) {
+commonValidator.install = function (Vue, options) {
 
-  Vue.prototype.$commonValidator['phoneValidator'] = function (rule, value, callback) {
+  let opt = {
+    defaultType: 'phone'
+  };
+
+  for (property in options) {
+    opt[property] = options[property]
+  };
+
+  Vue.prototype.$commonValidator = function (type, rule, value, callback) {
+
+    let curType = type ? type : opt.defaultType
+
+    switch (curType) {
+      case 'phone':
+        phone(rule, value, callback);
+        break;
+      case 'idcard':
+        idcard(rule, value, callback);
+        break;
+      case 'hello':
+        hello();
+        break;
+      default:
+        console.log('get the wrong type !')
+    }
+  };
+
+  ['phone', 'idcard', 'hello'].forEach(function (type){
+    Vue.prototype.$commonValidator[type] = function (rule, value, callback) {
+      return Vue.prototype.$commonValidator(type, rule, value, callback)
+    }
+  });
+
+  let hello = function () {
+    alert(phoneValidator.rule);
+  }
+
+  let phone = function (rule, value, callback) {
     if (!value) {
       return callback(new Error('手机号不能为空'));
-    } else if (!phone.rule.test(value)) {
+    } else if (!phoneValidator.rule.test(value)) {
       callback('手机号格式不正确');
     } else {
       callback();
     }
   }
 
+  let idcard = function (rule, value, callback) {
+
+  }
+
 }
 
-
-module.exports = commonValidator;
+export default commonValidator;
+// module.exports = commonValidator;
